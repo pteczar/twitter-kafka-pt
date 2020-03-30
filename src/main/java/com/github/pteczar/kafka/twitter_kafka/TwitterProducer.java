f2896d91b2f09c6336e17feef4771715ecd265e2
pteczar <p.teczar@gmail.com>
2020-03-30 21:04:32 +0200

    TwitterProducer / Idempotent producer - safe producer

--- src/main/java/com/github/pteczar/kafka/twitter_kafka/TwitterProducer.java
+++ src/main/java/com/github/pteczar/kafka/twitter_kafka/TwitterProducer.java
@@ -130,6 +130,13 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+     // making producer safe
+
+     properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+     properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+     properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+     properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5"); // our Kafka is 2.00 5 is a good choice
+
 
     //create the producer
 
